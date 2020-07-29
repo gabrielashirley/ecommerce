879476dc31b1d0a4b491b4f74474223674a7e135
Gabriela Shirley <gabrielashirleyh@gmail.com>
2020-07-29 16:21:11 -0700

    Initial setup with homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <soan className='subtitle'>SHOP NOW</soan>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>JACKET</h1>
+                        <soan className='subtitle'>SHOP NOW</soan>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SNEAKERS</h1>
+                        <soan className='subtitle'>SHOP NOW</soan>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MAN</h1>
+                        <soan className='subtitle'>SHOP NOW</soan>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMAN</h1>
+                        <soan className='subtitle'>SHOP NOW</soan>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default HomePage;
