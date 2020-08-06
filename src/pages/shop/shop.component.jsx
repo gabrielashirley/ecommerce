3216c98b9304cef8dcd611f7718325afef0e0514
Gabriela Shirley <gabrielashirleyh@gmail.com>
2020-08-06 12:39:13 -0700

    Collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import { Route } from 'react-router-dom';
 
-const ShopPage = ({ collections }) => (
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../../pages/collection/collection.component';
+
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <CollectionOverview />
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 
